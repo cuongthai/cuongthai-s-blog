f66f0f8316816c2db320fc4e081dfb58a7f985ae
steve <thaiylcuong1202@gmail.com>
2012-02-26 08:55:25 +0700

    First commit

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from blog.models import PostsSitemap
+from minicms.models import PagesSitemap
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+sitemaps = {
+    'posts': PostsSitemap,
+    'pages': PagesSitemap,
+}
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    (r'^admin/', include('urlsadmin')),
+    (r'^blog/', include('blog.urls')),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}),
+    (r'^search$', 'google_cse.views.search'),
+    (r'^robots\.txt$', 'robots.views.robots'),
+)
